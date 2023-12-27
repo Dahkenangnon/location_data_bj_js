39d2d3e755a8d211d2ecbb92e47d93715cff9dd5
Dah-kenangnon (Open to Remote Job) <dah.kenangnon@gmail.com>
2023-12-27 18:03:40 +0000

    Remove uncessary comments and update public/index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,20 @@
-// const path = require('path');
-
-// module.exports = {
-//     entry: './src/index.ts',
-//     output: {
-//         filename: 'bundle.js',
-//         path: path.resolve(__dirname, 'dist')
-//     },
-//     resolve: {
-//         extensions: ['.ts', '.js']
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.ts$/,
-//                 use: 'ts-loader',
-//                 exclude: /node_modules/
-//             }
-//         ]
-//     }
-// };
-
 const path = require('path');
 
 module.exports = {
-    //mode: 'production', // Enables optimizations for production like minification
     entry: './src/index.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public/dist'),
-        sourceMapFilename: "[name].js.map" // Generates source map files
+        sourceMapFilename: "[name].js.map" 
     },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public/dist'),
-        library: 'BjLocationData', // This is the name of your library
-        libraryTarget: 'umd', // Universal Module Definition
+        library: 'BjLocationData',
+        libraryTarget: 'umd', 
         umdNamedDefine: true
     },
-    devtool: 'source-map', // Generates source maps
+    devtool: 'source-map',
     resolve: {
         extensions: ['.ts', '.js']
     },
