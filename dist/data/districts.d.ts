92e63701bcf0f2af6c92b14bde09c27b89a3667b
Dahkenangnon <Dahkenangnon@users.noreply.github.com>
2026-05-10 11:54:06 +0000

    deploy: df0609b6fdaf851dc07d32814e22c128dbb35b8e

--- dist/data/districts.d.ts
+++ dist/data/districts.d.ts
@@ -1,9 +1,10 @@
 /**
  * @license
- * Copyright Justin Dah-kenangnon - All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2023-Present Justin Dah-kenangnon <dah.kenangnon@gmail.com>
  *
  * @link https://dah-kenangnon.com
- * For more details, see LICENSE.md file in root folder
+ * For more details, see LICENSE file in root folder
  */
 import { IDistrict } from "../types/interfaces";
 declare const districts: IDistrict[];
